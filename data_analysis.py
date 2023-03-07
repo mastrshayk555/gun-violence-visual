4bd2acc397b22231e3d1d34a07e0ed64dcdf8aa0
jeff <jeffreyheim55@gmail.com>
2023-03-06 22:54:49 -0500

    combine total pop for future handling

--- data_analysis.py
+++ data_analysis.py
@@ -40,7 +40,7 @@ def add_adjusted_date(df: pd.DataFrame):
     df['year_month'] = df['Incident Date'].apply(lambda x: reset_date(x))
 
 
-def group_by_date_state(df: pd.DataFrame):
+def group_by_date_state(df: pd.DataFrame, pop_df: pd.DataFrame):
     """
         Breaks down data into year groups, sums incidents by month and combines it all
         back together into a new df
@@ -52,7 +52,8 @@ def group_by_date_state(df: pd.DataFrame):
         df_state_group = spec_date_df.groupby(['state_abbrev'])[['n_killed', 'n_injured']].sum()
         df_state_group['date'] = date
         df_state_group['date'] = pd.to_datetime(df_state_group['date']).dt.date
-        df_list.append(df_state_group)
+        grouped_df = get_total_pop(df_state_group, pop_df, str(date.year))
+        df_list.append(grouped_df)
 
     concat_df = pd.concat(df_list)
     concat_df.reset_index(inplace=True)
@@ -65,22 +66,37 @@ def total_incidents(df: pd.DataFrame):
     df['total_incidents'] = df['n_killed'] + df['n_injured']
     
 
+def get_total_pop(gun_df: pd.DataFrame, pop_df: pd.DataFrame, year):
+    """For each state in each year, add total population for given row"""
+
+    pop_year = pop_df[['state_abbrev', str(year)]]
+    pop_year = pop_year.rename(columns={str(year): 'population'})
+    comb_df = gun_df.merge(pop_year, on='state_abbrev', how='left')
+    return comb_df
+
+
+def add_percent_col(df):
+    df['rate'] = 100 * df['total_incidents'] / df['population']
+
 
 def prep_data() -> pd.DataFrame:
-    df = pd.read_csv(gun_data, encoding = "ISO-8859-1", index_col='Incident ID')
+    gun_df = pd.read_csv(gun_data, encoding = "ISO-8859-1", index_col='Incident ID')
     pop_df = process_state_pop()
     
-    drop_unused(df)
-    convert_types(df)
-    add_state_abbr(df)
+    drop_unused(gun_df)
+    convert_types(gun_df)
+    add_state_abbr(gun_df)
     add_state_abbr(pop_df)
-    add_adjusted_date(df)
-    new_df = group_by_date_state(df)
-    total_incidents(new_df)
-    return new_df
+    add_adjusted_date(gun_df)
+    gun_df = group_by_date_state(gun_df, pop_df)
+    total_incidents(gun_df)
+    add_percent_col(gun_df)
+    gun_df = gun_df.sort_values(by='date')
+
+    print(gun_df['date'])
+    print(gun_df.loc[gun_df['date']=='2014-03-01'])
+    return gun_df
 
 
-#prep_data()
 if __name__ == '__main__':
-    df = prep_data()
-    #print(df)
+    prep_data()
